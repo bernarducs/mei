930a89dc7845e5cfca295c909b52c1ffd90cf47c
bernarducs <bernarducs@gmail.com>
2020-10-14 17:22:57 -0300

    refatorando

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from helpers import verifica_arquivo, verifica_dir, retorna_ufs, \
     del_arquivos_inuteis, exporta_csv, renomeia_arquivo
 
 
-def ufs_por_municipio_cnae(pasta):
+def ufs_por_municipio_cnae(pasta, visivel=False):
     ufs = retorna_ufs()
     for uf in ufs:
-        uf_por_municipio_cnae(pasta, uf=uf)
+        uf_por_municipio_cnae(pasta, uf=uf, headless=visivel)
 
 
-def uf_por_municipio_cnae(pasta, uf='PERNAMBUCO'):
+def uf_por_municipio_cnae(pasta, uf='PERNAMBUCO', headless=True):
     check = verifica_arquivo(pasta, uf)
     if not check:
         del_arquivos_inuteis(pasta)
         path_folder = verifica_dir(pasta)
-        driver = init_browser(path_folder, headless=False)
+        driver = init_browser(path_folder, headless=headless)
         tab_check = municipio_cnae(driver, uf=uf)
         if tab_check:
             exporta_csv(driver)
